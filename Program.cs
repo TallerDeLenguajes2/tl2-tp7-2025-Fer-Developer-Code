6380317efd9c2022e5cd5506e557fe8981222a9a
PeraltaRomeroFacundoRodolfo <facundoperalta217@gmail.com>
2025-10-31 10:02:29 -0300

    Estecompila

--- Program.cs
+++ Program.cs
@@ -2,9 +2,6 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
     // Comentarios XML
@@ -12,7 +9,11 @@ builder.Services.AddSwaggerGen(c =>
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
 });
-builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
