1fd85946784cea922331c2dc530ae567170ce656
Fernando <ibanez.fernando.2018@gmail.com>
2025-10-31 13:54:15 -0300

    Works

--- Controllers/PresupuestosController.cs
+++ Controllers/PresupuestosController.cs
@@ -1,22 +1,85 @@
 using Microsoft.AspNetCore.Mvc;
 using TP7.PresupuestoModel;
 using TP7.PresupuestosRepositorySpace;
-using TP7.ProductoRepositorySpace;
 using TP7.ProductosModel;
+using TP7.PresupuestosDetalleModel;
+using System;
+using System.Collections.Generic;
 
-namespace TP8.Controllers;
+namespace TP7.Controllers;
 
-public class PresupuestoController : Controller
+[ApiController]
+[Route("api/[controller]")]
+public class PresupuestoController : ControllerBase
 {
-    private PresupuestosRepository presupuestoRepository;
+    private readonly PresupuestosRepository _presupuestoRepository;
+
     public PresupuestoController()
     {
-        presupuestoRepository = new PresupuestosRepository();
+        _presupuestoRepository = new PresupuestosRepository();
     }
-    [HttpGet]
-    public IActionResult Index()
+
+    [HttpPost]
+    public ActionResult<Presupuesto> CrearPresupuesto(Presupuesto presupuesto)
     {
-        List<Presupuesto> presupuestos = presupuestoRepository.GetAll();
-        return View(presupuestos);
+        try
+        {
+            var nuevoPresupuesto = _presupuestoRepository.Create(presupuesto);
+            return Ok(nuevoPresupuesto);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost("{id}/ProductoDetalle")]
+    public IActionResult AgregarProductoAPresupuesto(int id, [FromBody] PresupuestosDetalle detalle)
+    {
+        try
+        {
+            _presupuestoRepository.AgregarProductoAPresupuesto(id, detalle.Producto.IdProducto, detalle.Cantidad);
+            return Ok("Producto agregado al presupuesto exitosamente");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Presupuesto> GetById(int id)
+    {
+        var presupuesto = _presupuestoRepository.GetById(id);
+        if (presupuesto == null)
+        {
+            return NotFound("Presupuesto no encontrado");
+        }
+        return Ok(presupuesto);
+    }
+
+    [HttpGet]
+    public ActionResult<List<Presupuesto>> GetAll()
+    {
+        var presupuestos = _presupuestoRepository.GetAll();
+        return Ok(presupuestos);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult EliminarPresupuesto(int id)
+    {
+        try
+        {
+            var resultado = _presupuestoRepository.Delete(id);
+            if (resultado)
+            {
+                return Ok("Presupuesto eliminado exitosamente");
+            }
+            return NotFound("Presupuesto no encontrado");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 }
